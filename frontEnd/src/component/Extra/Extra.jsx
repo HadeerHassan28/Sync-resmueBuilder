d957481b43eca397e5323aa4c8d2bc5eed187deb
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-10-30 12:29:08 +0200

    some changes in extra

--- frontEnd/src/component/Extra/Extra.jsx
+++ frontEnd/src/component/Extra/Extra.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Extra.module.css";
 const Extra = ({ formData, setFormData }) => {
   // extra_1: "",
   //   extra_2: "",
-  const [showExtra, setshowExtra] = useState(false);
-  const addExtra = (e) => {
-    e.preventDefault();
-    setshowExtra(true);
-  };
+
   return (
     <>
       <form className="row g-3 imgHome">
@@ -15,7 +11,7 @@ const Extra = ({ formData, setFormData }) => {
         {/* extra_1 */}
         <div className="col-md-12">
           <label htmlFor="extra_1" className="form-label">
-            Extra
+            Languages
           </label>
           <input
             type="text"
@@ -29,41 +25,24 @@ const Extra = ({ formData, setFormData }) => {
           />
         </div>
 
-        {/* To add Second */}
-        <div className="d-flex justify-content-center ">
-          <button onClick={addExtra} className="btn px-4 py-1">
-            {" "}
-            <i className="fa-solid fa-circle-plus"></i>
-          </button>
+        <div className="col-md-12 ">
+          <label htmlFor="extra_2" className="form-label">
+            Hobbies
+          </label>
+          <input
+            type="text"
+            className="form-control "
+            id="extra_2"
+            name="extra_2"
+            value={formData.extra_2}
+            onChange={(e) => {
+              setFormData({
+                ...formData,
+                extra_2: e.target.value,
+              });
+            }}
+          />
         </div>
-
-        {showExtra ? (
-          <>
-            {/* 2nd Extra */}
-            {/* extra_2 */}
-            <h6 className="title mt-1">
-              <u>2nd Extra</u>
-            </h6>
-            <div className="col-md-12 mt-1">
-              <label htmlFor="extra_2" className="form-label">
-                Extra
-              </label>
-              <input
-                type="text"
-                className="form-control "
-                id="extra_2"
-                name="extra_2"
-                value={formData.extra_2}
-                onChange={(e) => {
-                  setFormData({
-                    ...formData,
-                    extra_2: e.target.value,
-                  });
-                }}
-              />
-            </div>
-          </>
-        ) : null}
       </form>
     </>
   );
